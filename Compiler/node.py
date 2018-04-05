342a7b56749a5d49ed1b7d192ffd53809003dba0
juangro <ing.juangro@gmail.com>
2018-04-05 12:50:31 -0500

    Implement TREEgit add -A

--- Compiler/node.py
+++ Compiler/node.py
@@ -1,11 +1,23 @@
 class Node:
-    def __init__(self,type,children=None,leaf=None):
-         self.type = type
-         if children:
-              self.children = children
-         else:
-              self.children = [ ]
-         self.leaf = leaf
+    def __init__(self, token, value=None, ntype=None, children=None):
+            self.token = token
+            self.value = value
+            self.ntype = ntype
+            if children:
+                self.children = children
+            else:
+                self.children = []
+
+    def get_values(self):
+        string = str(self.token)
+        # if self.value:
+        #     string = string + ':' + str(self.value)
+        if self.ntype:
+            string = string + ':' + str(self.ntype)
+        return string
 
     def print_node(self):
-        print(self.type)
+        if self.children:
+            for child in self.children:
+                print(self.get_values(), '->', child.get_values())
+                child.print_node()
--- Compiler/parser.py
+++ Compiler/parser.py
@@ -6,18 +6,22 @@ import ply.yacc as yacc
 # Get the token map
 tokens = lex.tokens
 
+def p_program(t):
+    'program : statement_list'
+    t[0] = Node("program", None, None, [t[1]])
+
 # statement-list
 def p_statement_list_1(t):
     'statement_list : empty'
-    t[0] = Node('statement-list', t[1])
+    t[0] = Node("statement_list", None, None, None)
 
 def p_statement_list_2(t):
     'statement_list : statement'
-    t[0] = Node('statement-list', t[1])
+    t[0] = Node("statement_list", None, None, [t[1]])
 
 def p_statement_list_3(t):
     'statement_list : statement_list statement'
-    t[0] = Node('statement-list', t[2])
+    t[0] = Node("statement_list", None, None, [t[1], t[2]])
 
 # statement
 def p_statement(t):
@@ -26,120 +30,143 @@ def p_statement(t):
               | selection_statement
               | iteration_statement
     '''
-    t[0] = Node('statement', t[1])
+    t[0] = Node("statement", None, None, [t[1]])
 
 # expression-statement
 def p_expression_statement_1(t):
     'expression_statement : declaration_expression SEMI'
-    t[0] = Node('expression-statement', t[1], t[2])
+    T = Node(t[2], t[2])
+    t[0] = Node("expression_statement", None, None, [t[1], T])
 
 def p_expression_statement_2(t):
     'expression_statement : empty SEMI'
-    t[0] = Node('expression-statement', t[1], t[2])
+    T = Node(t[2], t[2])
+    t[0] = Node("expression_statement", None, None, [t[1], T])
 
 def p_expression_statement_3(t):
     'expression_statement : input_expression SEMI'
-    t[0] = Node('expression-statement', t[1], t[2])
+    T = Node(t[2], t[2])
+    t[0] = Node("expression_statement", None, None, [t[1], T])
 
 def p_expression_statement_4(t):
     'expression_statement : output_expression SEMI'
-    t[0] = Node('expression-statement', t[1], t[2])
+    T = Node(t[2], t[2])
+    t[0] = Node("expression_statement", None, None, [t[1], T])
 
 # declaration-expression
 def p_declaration_expression_1(t):
     'declaration_expression : assignment_expression'
-    t[0] = Node('declaration-expression', t[1])
+    t[0] = Node("declaration_expression", None, None, [t[1]])
 
 def p_declaration_expression_2(t):
     'declaration_expression : type_specifier declaration_expression'
-    t[0] = Node('declaration-expression', t[2], t[1])
+    t[0] = Node("declaration_expression", None, None, [t[1], t[2]])
 
 # selection-statement
 def p_selection_statement_1(t):
     'selection_statement : IF conditional_expression block_statement_list'
-    t[0] = Node('selection-statement', [t[2], t[3]], t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node("selection_statement", None, None, [T, t[2], t[3]])
 
 def p_selection_statement_2(t):
     'selection_statement : selection_statement ELSE block_statement_list'
-    t[0] = Node('selection-statement', t[3], t[2])
+    T = Node(t[2], t[2])
+    t[0] = Node("selection_statement", None, None, [t[1], T, t[3]])
 
 # iteration-statement:
 def p_iteration_statement_1(t):
     'iteration_statement : WHILE conditional_expression block_statement_list'
-    t[0] = Node('iteration-statement', [t[2], t[3]], t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node("iteration_statement", None, None, [T, t[2], t[3]])
 
 def p_iteration_statement_2(t):
     'iteration_statement : DO block_statement_list WHILE conditional_expression'
-    t[0] = Node('iteration-statement', [t[2], t[4]], [t[1], t[3]])
+    T1 = Node(t[1], t[1])
+    T2 = Node(t[3], t[3])
+    t[0] = Node("iteration_statement", None, None, [T1, t[2], T2, t[4]])
 
 # block-statement-list
 def p_block_statement_list_1(t):
     'block_statement_list : LBRACE statement_list RBRACE'
-    t[0] = Node('block-statement-list', t[2], [t[1], t[3]])
+    T1 = Node(t[1], t[1])
+    T2 = Node(t[3], t[3])
+    t[0] = Node("block_statement_list", None, None, [T1, t[2], T2])
 
 # conditional-statement
 def p_conditional_expression_1(t):
     'conditional_expression : LPAREN logical_expression RPAREN'
-    t[0] = Node('conditional-expression', t[2], [t[1], t[3]])
+    T1 = Node(t[1], t[1])
+    T2 = Node(t[3], t[3])
+    t[0] = Node("conditional_expression", None, None, [T1, t[2], T2])
 
 # assignment-expression
 def p_assignment_expression_1(t):
     'assignment_expression : variable_expression'
-    t[0] = Node('conditional-expression', t[1])
+    t[0] = Node("assignment_expression", None, None, [t[1]])
 
 def p_assignment_expression_2(t):
     'assignment_expression : assignment_expression EQUALS logical_expression'
-    t[0] = Node('conditional-expression', t[3], t[2])
+    T = Node(t[2], t[2])
+    t[0] = Node("assignment_expression", None, None, [t[1], T, t[3]])
 
 def p_assignment_expression_3(t):
     'assignment_expression : assignment_expression EQUALS input_expression'
-    t[0] = Node('conditional-expression', t[3], t[2])
+    T = Node(t[2], t[2])
+    t[0] = Node("assignment_expression", None, None, [t[1], T, t[3]])
 
 # logical-expression:
 def p_logical_expression_1(t):
     'logical_expression : equality_expression'
-    t[0] = Node('conditional-expression', t[1])
+    t[0] = Node("logical_expression", None, None, [t[1]])
 
 def p_logical_expression_2(t):
     'logical_expression : logical_expression logical_operators equality_expression'
-    t[0] = Node('conditional-expression', t[3], t[2])
+    t[0] = Node("logical_expression", None, None, [t[1], t[2], t[3]])
 
 # equality-expression:
 def p_equality_expression_1(t):
     'equality_expression : relational_expression'
-    t[0] = Node('conditional-expression', t[1])
+    t[0] = Node("equality_expression", None, None, [t[1]])
 
 def p_equality_expression_2(t):
     'equality_expression : equality_expression equality_operators relational_expression'
-    t[0] = Node('conditional-expression', t[3], t[2])
+    t[0] = Node("equality_expression", None, None, [t[1], t[2], t[3]])
 
 # relational-expression:
 def p_relational_expression_1(t):
     'relational_expression : math_expression'
-    t[0] = Node('conditional-expression', t[1])
+    t[0] = Node("relational_expression", None, None, [t[1]])
 
 def p_relational_expression_2(t):
     'relational_expression : relational_expression relational_operators math_expression'
-    t[0] = Node('conditional-expression', t[3], t[2])
+    t[0] = Node("relational_expression", None, None, [t[1], t[2], t[3]])
 
 # math-expression
 def p_math_expression_1(t):
     'math_expression : primary_expression'
-    t[0] = Node('conditional-expression', t[1])
+    t[0] = Node("math_expression", None, None, [t[1]])
 
 def p_math_expression_2(t):
     'math_expression : math_expression math_operators primary_expression'
-    t[0] = Node('conditional-expression', t[3], t[2])
+    t[0] = Node("math_expression", None, None, [t[1], t[2], t[3]])
 
 # input-expression
 def p_input_expression(t):
     'input_expression : CONSOLE DOT READLINE LPAREN RPAREN'
-    t[0] = Node('conditional-expression', [t[1], t[2], t[3], t[4], t[5]])
+    T1 = Node(t[1], t[1])
+    T2 = Node(t[2], t[2])
+    T3 = Node(t[3], t[3])
+    T4 = Node(t[4], t[4])
+    T5 = Node(t[5], t[5])
+    t[0] = Node("input_expression", None, None, [T1, T2, T3, T4, T5])
 
 # output-expression
 def p_output_expression(t):
     'output_expression : CONSOLE DOT WRITELINE conditional_expression'
-    t[0] = Node('conditional-expression', t[4], [t[1], t[2], t[3]])
+    T1 = Node(t[1], t[1])
+    T2 = Node(t[2], t[2])
+    T3 = Node(t[3], t[3])
+    t[0] = Node("output_expression", None, None, [T1, T2, T3, t[4]])
 
 ''' TERMINALS '''
 # type-specifier
@@ -149,7 +176,8 @@ def p_type_specifier(t):
                    | BOOL
                    | STRING
     '''
-    t[0] = Node('type-specifier', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('type_specifier', None, None, [T])
 
 # logical-operators
 def p_logical_operators(t):
@@ -157,7 +185,8 @@ def p_logical_operators(t):
     logical_operators : LAND
                       | LOR
     '''
-    t[0] = Node('logical-operators', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('logical_operators', None, None, [T])
 
 # equality-operators
 def p_equality_operators(t):
@@ -165,7 +194,8 @@ def p_equality_operators(t):
     equality_operators : EQ
                        | NE
     '''
-    t[0] = Node('equality-operators', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('equality_operators', None, None, [T])
 
 # relational-operators
 def p_relational_operators(t):
@@ -175,7 +205,8 @@ def p_relational_operators(t):
                          | LE
                          | GE
     '''
-    t[0] = Node('relational-operators', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('relational_operators', None, None, [T])
 
 # math-operators
 def p_math_operators(t):
@@ -185,17 +216,19 @@ def p_math_operators(t):
                    | TIMES
                    | DIVIDE
     '''
-    t[0] = Node('math-operators', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('math_operators', None, None, [T])
 
 # primary-expression
 def p_primary_expression(t):
     '''
     primary_expression :  variable_expression
+                       |  boolean_expression
                        |  DIGIT
                        |  STRING_SENTENCE
-                       |  boolean_expression
     '''
-    t[0] = Node('primary-expression', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('primary_expression', None, None, [T])
 
 # variable-expression
 def p_variable_expression(t):
@@ -203,7 +236,8 @@ def p_variable_expression(t):
     variable_expression : ID
                         | CONSTANT
     '''
-    t[0] = Node('variable-expression', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('variable_expression', None, None, [T])
 
 # boolean-expression
 def p_boolean_expression(t):
@@ -211,11 +245,12 @@ def p_boolean_expression(t):
     boolean_expression : TRUE
                        | FALSE
     '''
-    t[0] = Node('boolean-expression', t[1])
+    T = Node(t[1], t[1])
+    t[0] = Node('boolean_expression', None, None, [T])
 
 def p_empty(t):
     'empty : '
-    pass
+    t[0] = Node('empty', None, None, None)
 
 def p_error(t):
     print("ERROR:", t.value)
