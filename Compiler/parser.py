c6da1673b47fb4faed285fa57808cf1e80e9d6d9
juangro <ing.juangro@gmail.com>
2018-04-05 13:38:23 -0500

    Finish second delivery

--- Compiler/parser.py
+++ Compiler/parser.py
@@ -223,8 +223,7 @@ def p_math_operators(t):
 def p_primary_expression(t):
     '''
     primary_expression :  variable_expression
-                       |  TRUE
-                       |  FALSE
+                       |  boolean_expression
                        |  DIGIT
                        |  STRING_SENTENCE
     '''
@@ -239,6 +238,14 @@ def p_variable_expression(t):
     '''
     T = Node(t[1], t[1])
     t[0] = Node('variable_expression', None, None, [T])
+
+def p_boolean_expression(t):
+    '''
+    boolean_expression : TRUE
+                       | FALSE
+    '''
+    T = Node(t[1], t[1])
+    t[0] = Node('boolean_expression', None, None, [T])
 
 def p_empty(t):
     'empty : '
